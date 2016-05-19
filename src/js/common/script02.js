4085bb8737d74c1469ee0a9992bfda91ac2d829e
Hiraishi Yoko <hiraishi-y@gnavi.co.jp>
2016-05-19 10:55:11 +0900

    practice02 clickイベントを登録

--- src/js/common/script02.js
+++ src/js/common/script02.js
@@ -5,21 +5,27 @@
  *****------------------------------------------------------------------*/
 (function () {
   'use strict';
-  
+
   // shortcut
   var doc = document;
-  
+
   /**
    * native js
    *--------------------------------------------------*/
-  
-  
-  // 〜 practice02の課題をここに記述 〜
-  
-  
-  
-})();
 
+   LESSON.dialogBox = {
 
+     buttons: doc.getElementsByTagName('button')
 
+  };
 
+  // button 要素に、自信のラベルテキストをアラートで表示する click イベントを付与する
+  for (var i = 0, max = LESSON.dialogBox.buttons.length; i < max; i++){
+    LESSON.dialogBox.buttons[i].addEventListener('click', function (e) {
+      e.preventDefault();
+
+      window.alert(this.innerText);
+    }, false);
+  }
+
+})(window.LESSON = window.LESSON || {});
