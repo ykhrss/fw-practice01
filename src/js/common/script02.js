980583226748930142776ba06e85f25d586feed5
Hiraishi Yoko <hiraishi-y@gnavi.co.jp>
2016-05-20 11:21:24 +0900

    practice02 要素の判定を追加

--- src/js/common/script02.js
+++ src/js/common/script02.js
@@ -12,20 +12,51 @@
   /**
    * native js
    *--------------------------------------------------*/
+   LESSON.dialogBoxS02 = {
 
-   LESSON.dialogBox = {
+     // button 要素を取得する
+     $buttonElem: doc.getElementsByTagName('button'),
 
-     buttons: doc.getElementsByTagName('button')
+     // button 要素を wrap している要素を取得する
+     $buttonWrappingElem: doc.getElementsByTagName('p'),
+
+     init: function () {
+       // HTML に button要素が存在していたら `addClickEventButtonElem`を実行する
+       if (this.$buttonElem.length > 0) {
+         this.addClickEventButtonElem();
+       }
+     },
+
+     // 子要素に button 要素があるかどうかを確認する
+     hasButtonElem: function ($elem) {
+       // 要素の class 値を取得
+       var classValueDefault = $elem.className,
+        classNameWrappingButton = 'js-button-native';
+
+       // class 値に `js-button-native` を含んでいるか、真偽値を返す
+       return classValueDefault.indexOf(classNameWrappingButton);
+     },
+
+     // button 要素に、自信のラベルテキストをアラートで表示する click イベントを付与する
+     addClickEventButtonElem: function () {
+       // button 要素を wrap している要素を全て検索する
+       for (var i = 0, max = this.$buttonWrappingElem.length; i < max; i++) {
+
+         // 子要素に button 要素が存在している場合、click イベントを登録する
+         if (this.hasButtonElem(this.$buttonWrappingElem[i]) > 0) {
+           var $button = this.$buttonWrappingElem[i].getElementsByTagName('button')[0];
+
+           $button.addEventListener('click', function (e) {
+             e.preventDefault();
+
+             window.alert(this.innerText);
+           }, false);
+         }
+       }
+     }
 
   };
 
-  // button 要素に、自信のラベルテキストをアラートで表示する click イベントを付与する
-  for (var i = 0, max = LESSON.dialogBox.buttons.length; i < max; i++){
-    LESSON.dialogBox.buttons[i].addEventListener('click', function (e) {
-      e.preventDefault();
-
-      window.alert(this.innerText);
-    }, false);
-  }
+  LESSON.dialogBoxS02.init();
 
 })(window.LESSON = window.LESSON || {});
