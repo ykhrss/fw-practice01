e3f95b40e34b8a4aeb58b9a7dcfab901b06b0a4d
Hiraishi Yoko <hiraishi-y@gnavi.co.jp>
2016-06-10 14:08:21 +0900

    * for文 カウンタ変数の更新処理を追加
    - 省略はNG
    - カウンタ変数に親ノードを代入して反復処理を行うことで親ノードを１つずつ遡って処理をしている

--- src/js/common/script02.js
+++ src/js/common/script02.js
@@ -24,14 +24,18 @@
   };
 
   doc.addEventListener('click', function(e) {
-    for (var $clickedItem = e.target; $clickedItem && $clickedItem !== this;) {
+    // click 要素から親要素に遡って処理をする
+    for (var $clickedItem = e.target; $clickedItem && $clickedItem !== this;　$clickedItem = $clickedItem.parentNode) {
 
-      // click 要素の親ノードに 特定の class 属性値が存在していたら
-      // 自信のラベルテキストをアラート表示する
-      if ($clickedItem.parentNode.classList.contains('js-button-native')) {
+      // click 要素の親ノードに 特定の class 属性値が存在しているか判定
+      if ($clickedItem.parentNode.classList && $clickedItem.parentNode.classList.contains('js-button-native')) {
 
+        // 自信のラベルテキストをアラート表示する
         LESSON.dialogBoxS02.alertButtonText($clickedItem, e);
+
+        // ループをぬける
         break;
+
       }
     }
   }, false);
